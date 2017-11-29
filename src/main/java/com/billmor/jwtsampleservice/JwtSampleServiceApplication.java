15a91018282cc1b6db1bea4172ae525de396392a
bill-mor <bmoran987@gmail.com>
2017-11-28 19:46:46 -0500

    Initial Commit

---
+++ src/main/java/com/billmor/jwtsampleservice/JwtSampleServiceApplication.java
@@ -0,0 +1,12 @@
+package com.billmor.jwtsampleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSampleServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSampleServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/billmor/jwtsampleservice/JwtSampleServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.billmor.jwtsampleservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JwtSampleServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
